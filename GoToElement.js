eb39216622b36979916d43a6cfb217b2059fbee4
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-12 13:02:33 +0800

    123

--- GoToElement.js
+++ GoToElement.js
@@ -1,12 +1,19 @@
-window.onload=()=>{
-    console.log("頁面載入完成~");
-}
-var test= document.getElementById("test");
+window.onload = () => {
 
-test.onclick=() =>{
-    var target= test.getAttribute("data-gte-target");
-    var offset= test.getAttribute("data-gte-offset");
+    // 取得元素
+    var test = document.getElementById("test");
 
-    console.log(target);
-    console.log(offset);
+    // 元素.點擊事件
+    test.onclick = () => {
+        var target = test.getAttribute("data-gte-target"); //目標屬性
+        var offset = test.getAttribute("data-gte-offset"); //位移屬性
+
+        var element = document.getElementById(target);  //目標元素
+
+        // 視窗.捲動至({上方，行為})
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior: "smooth"
+        })
+    }
 }
