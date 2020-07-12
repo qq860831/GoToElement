41a4806580aa962ae48e65939bf32e8bb9926566
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-12 14:36:21 +0800

    ok

--- GoToElement.js
+++ GoToElement.js
@@ -1,19 +1,26 @@
 window.onload = () => {
 
     // 取得元素
-    var test = document.getElementById("test");
+    var elements = document.getElementsByTagName("*");
 
-    // 元素.點擊事件
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target"); //目標屬性
-        var offset = test.getAttribute("data-gte-offset"); //位移屬性
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
+        // .代表是取得資料
 
-        var element = document.getElementById(target);  //目標元素
+        // 元素.點擊事件
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target"); //目標屬性
+            var offset = e.getAttribute("data-gte-offset"); //位移屬性
 
-        // 視窗.捲動至({上方，行為})
-        window.scrollTo({
-            top: element.offsetTop,
-            behavior: "smooth"
-        })
+            if (target) {
+                var element = document.getElementById(target);  //目標元素
+
+                // 視窗.捲動至({上方，行為})
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior: "smooth"
+                })
+            }
+        }
     }
 }
